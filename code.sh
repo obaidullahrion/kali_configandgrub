b61646df5cff257eea7fe87681291a6510b82987
obaidullah <79790901+obaidullahrion@users.noreply.github.com>
2025-02-05 04:37:16 -0500

    Delete code.sh

--- code.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install software-properties-common apt-transport-https curl -y
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt update
-sudo apt install code -y
